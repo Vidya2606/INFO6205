b9e2c66057f3e4b546de004160dab9d8fcd59996
Vidya <59590642+Vidya2606@users.noreply.github.com>
2020-09-14 23:22:59 -0700

    Update 1. SumOfTwoStrings.java

--- CodeLab/Week 1/1. SumOfTwoStrings.java
+++ CodeLab/Week 1/1. SumOfTwoStrings.java
@@ -3,7 +3,11 @@ Given two non-negative integers num1 and num2 represented as string,return the s
  */
 
 public int sumOfTwoStrings(String n1, String n2){
-    int num1 = (n1 != null && n1 != "") ? Integer.parseInt(n1) : 0;
+    int num1 = (n1 != null && n1 != "") ? Integer.parseInt(n1) : 0; 
     int num2 = (n2 != null && n2 != "") ? Integer.parseInt(n2) : 0;
     return num1 + num2;
 }
+// Comments: 
+// Return the result in String format, also handle the carry 
+// public int[] sumOfTwoStrings(String n1, String n2) {
+//}
